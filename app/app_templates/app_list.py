0c5533c631318cb07528a6fc34635c2f71d2988e
linusidom <linusidom@gmail.com>
2019-04-23 16:17:29 +0700

    updated the login.html to remove signup and accounts

--- app/app_templates/app_list.py
+++ app/app_templates/app_list.py
@@ -9,11 +9,8 @@ def app_list(app_name, dst):
 <hr>\n\
 {% if "+app_name[0:-1]+"_list %}\n\
 	{% for "+app_name[0:-1]+" in "+app_name[0:-1]+"_list %}\n\
-		<a href=\"{% url '"+app_name+":"+app_name[0:-1]+"_detail' slug="+app_name[0:-1]+".slug %}\"><p>{{"+app_name[0:-1]+".user}}</p></a>\n\
-		<p>{{"+app_name[0:-1]+".slug}}</p>\n\
+		<a href=\"{% url '"+app_name+":"+app_name[0:-1]+"_detail' slug="+app_name[0:-1]+".slug %}\"><p>{{"+app_name[0:-1]+".slug}}</p></a>\n\
 		<p>{{"+app_name[0:-1]+".name}}</p>\n\
-		<p>{{"+app_name[0:-1]+".address}}</p>\n\
-		<p>{{"+app_name[0:-1]+".description}}</p>\n\
 	{% endfor %}\n\
 {% else %}\n\
 <p>No Locations</p>\n\
